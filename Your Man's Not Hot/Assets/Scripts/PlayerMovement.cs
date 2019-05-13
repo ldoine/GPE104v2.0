f66aaa737cc35f3e86d203efa2ced5c9846ff2f5
ldoine <logan.doine@gmail.com>
2019-05-12 22:39:19 -0700

    Sprite Mover

---
+++ Your Man's Not Hot/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    //This float is used to set the players movement speed
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //These if statements are used to move the player when either the "A" key or "D" key are pressed
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+    }
+}
